0e25411e18eef64bf7d30fb5c6f921faacd0930b
kurstah <nahuelparoldo@hotmail.com>
2023-07-17 17:20:09 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,21 +5,19 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-        path('https://nahuelparold-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/', views.my_view, name='my_view'), 
-
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
 
-    # path for about view
     path(route='about', view=views.about, name='about'),
-    # path for contact us view
+
     path(route='contact', view=views.contact, name='contact'),
-    # path for registration
 
-    # path for login
+    path(route='registration', view=views.registration_request, name='registration'),
 
-    # path for logout
+    path(route='login', view=views.login_request, name='login'),
+
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
